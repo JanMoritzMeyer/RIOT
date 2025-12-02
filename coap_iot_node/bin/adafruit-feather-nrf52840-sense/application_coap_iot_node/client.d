c2586fb3cba2e3e27f6ff1f1c312cbb263a7b771
Martin <schnatzyfire@googlemail.com>
2025-12-02 13:36:55 +0100

    removed bin

--- coap_iot_node/bin/adafruit-feather-nrf52840-sense/application_coap_iot_node/client.d
+++
@@ -1,4 +0,0 @@
-/home/xostra/workspace/iot_firmware_app/coap_iot_node/bin/adafruit-feather-nrf52840-sense/application_coap_iot_node/client.o: \
- /home/xostra/workspace/iot_firmware_app/coap_iot_node/client.c \
- /home/xostra/workspace/iot_firmware_app/coap_iot_node/bin/adafruit-feather-nrf52840-sense/riotbuild/riotbuild.h
-/home/xostra/workspace/iot_firmware_app/coap_iot_node/bin/adafruit-feather-nrf52840-sense/riotbuild/riotbuild.h:
--- coap_iot_node/bin/adafruit-feather-nrf52840-sense/random/musl_lcg.d
+++
@@ -1,6 +0,0 @@
-/home/xostra/workspace/iot_firmware_app/coap_iot_node/bin/adafruit-feather-nrf52840-sense/random/musl_lcg.o: \
- /home/xostra/workspace/iot_firmware_app/RIOT/sys/random/musl_lcg.c \
- /home/xostra/workspace/iot_firmware_app/coap_iot_node/bin/adafruit-feather-nrf52840-sense/riotbuild/riotbuild.h \
- /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h
-/home/xostra/workspace/iot_firmware_app/coap_iot_node/bin/adafruit-feather-nrf52840-sense/riotbuild/riotbuild.h:
-/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h:
